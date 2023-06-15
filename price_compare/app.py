470a14e282c0074f9171ea9d02e6839b493ba147
sudhanshuj9 <90786688+sudhanshuj9@users.noreply.github.com>
2023-06-15 18:45:14 +0530

    Add files via upload

--- price_compare/app.py
+++ price_compare/app.py
@@ -22,17 +22,17 @@ def flipkart(name):
             flipkart_name = soup.select('._4rR01T')[0].getText().strip()
             if name.upper() in flipkart_name.upper():
                 flipkart_price = soup.select('._30jeq3')[0].getText().strip()
-                return flipkart_name, flipkart_price
+                return flipkart_name, flipkart_price, flipkart_url
 
         elif soup.select('.s1Q9rs'):
             flipkart_name = soup.select('.s1Q9rs')[0].getText().strip()
             if name.upper() in flipkart_name.upper():
                 flipkart_price = soup.select('._30jeq3')[0].getText().strip()
-                return flipkart_name, flipkart_price
+                return flipkart_name, flipkart_price, flipkart_url
 
-        return None, '0'
+        return None, '0', None
     except:
-        return None, '0'
+        return None, '0', None
 
 
 def amazon(name):
@@ -51,11 +51,11 @@ def amazon(name):
             amazon_name = soup.select('.a-color-base.a-text-normal')[i].getText().strip()
             if name in amazon_name.upper():
                 amazon_price = soup.select('.a-price-whole')[i].getText().strip()
-                return amazon_name, amazon_price
+                return amazon_name, amazon_price, amazon_url
 
-        return None, '0'
+        return None, '0', None
     except:
-        return None, '0'
+        return None, '0', None
 
 
 def convert(price):
@@ -67,25 +67,35 @@ def convert(price):
 def index():
     if request.method == 'POST':
         product_name = request.form.get('product_name')
-        flipkart_name, flipkart_price = flipkart(product_name)
-        amazon_name, amazon_price = amazon(product_name)
-
-        if flipkart_name is None:
-            flipkart_name = "No product found!"
-            flipkart_price = '0'
-        else:
-            flipkart_price = convert(flipkart_price)
-
-        if amazon_name is None:
-            amazon_name = "No product found!"
-            amazon_price = '0'
-        else:
-            amazon_price = convert(amazon_price)
-
-        return render_template('index.html', flipkart_name=flipkart_name, flipkart_price=flipkart_price,
-                               amazon_name=amazon_name, amazon_price=amazon_price)
-
+        return render_template('results.html', product_name=product_name)
     return render_template('index.html')
+
+
+@app.route('/results', methods=['POST'])
+def results():
+    product_name = request.form.get('product_name')
+    flipkart_name, flipkart_price, flipkart_url = flipkart(product_name)
+    amazon_name, amazon_price, amazon_url = amazon(product_name)
+
+    if flipkart_name is None:
+        flipkart_name = "No product found!"
+        flipkart_price = '0'
+        flipkart_url = None
+    else:
+        flipkart_price = convert(flipkart_price)
+
+    if amazon_name is None:
+        amazon_name = "No product found!"
+        amazon_price = '0'
+        amazon_url = None
+    else:
+        amazon_price = convert(amazon_price)
+
+    price_difference = flipkart_price - amazon_price
+
+    return render_template('results.html', flipkart_name=flipkart_name, flipkart_price=flipkart_price,
+                           amazon_name=amazon_name, amazon_price=amazon_price, price_difference=price_difference,
+                           flipkart_url=flipkart_url, amazon_url=amazon_url)
 
 
 if __name__ == '__main__':
